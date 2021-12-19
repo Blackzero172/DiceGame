98c4dd373f603d772c572c315164906edb41f896
BlackZero172 <Supermeega@outlook.com>
2021-12-19 09:14:30 +0200

    .

--- src/components/Counter/Counter.components.jsx
+++ src/components/Counter/Counter.components.jsx
@@ -4,7 +4,7 @@ class Counter extends React.Component {
 	render() {
 		const { playerNumber, isTemp } = this.props;
 		const counterClass = isTemp ? `temp-counter-${playerNumber}` : `counter-${playerNumber}`;
-		const counterText = isTemp ? `Temp Score:` : `Total Score:`;
+		const counterText = isTemp ? `Current Score:` : `Total Score:`;
 		return (
 			<div className={counterClass}>
 				<h3>
